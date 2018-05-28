ceecd6ed6ea56da0f925e526d17304720896a8e1
Ha <victorha@gmail.com>
2018-05-28 18:55:17 -0400

    changed from bandsintown to eventful

--- assets/javascript/application.js
+++ assets/javascript/application.js
@@ -1,6 +1,7 @@
 var city = $("#city").val();
-var artist = $("#artist").val();
-var artistNoSpace = artist.replace(" ", "%20")
+var artist = $("#artist").val().trim();
+//var bandsArtistNoSpace = artist.replace(" ", "%20") //changes spaces to bandsInTown's format
+var eventArtistNoSpace = artist.replace(" ", "+") //changes spaces to eventful's format
 
 $(document).ready(function() {
     console.log("Ready!");
@@ -26,32 +27,46 @@ $(document).ready(function() {
       };
 
 
-
-
-function bandsintown() {
-    var bandsURL = "rest.bandsintown.com/artists/"+artistNoSpace+"/events?app_id="
+function eventful() {
+    var eventfulURL = "http://api.eventful.com/json/events/search?app_key=BMHGt9rHhxJ8frMs&location="+city+"&keywords="+artist
 
     $.ajax ({
-        url: bandsURL,
+        url: eventfulURL,
         method: "GET"
     }).then(function(response) {
-        for (s=0; s<response.length; s++) {
-            var town = response.venue.city;
-            if (town ==city) {
-                console.log(response[s].venue.name);
-                console.log(response[s].datetime);
-            }
+        console.log(response)
+        for (e = 0; e < response.events.event.length; e++) {
+            console.log(response.events.event[e].title)
+            console.log(response.events.event[e].venue_address)
+            console.log(response.events.event[e].start_time)
         }
     })
 }
+
+// function bandsintown() {
+//     var bandsURL = "rest.bandsintown.com/artists/"+bandsArtistNoSpace+"/events?app_id="
+
+//     $.ajax ({
+//         url: bandsURL,
+//         method: "GET"
+//     }).then(function(response) {
+//         for (s=0; s<response.length; s++) {
+//             var town = response.venue.city;
+//             if (town ==city) {
+//                 console.log(response[s].venue.name);
+//                 console.log(response[s].datetime);
+//             }
+//         }
+//     })
+// }
 
 // ---------------------
 
 
 
 for (a= 0; a < similarArtists.length; a++) {
-    artistNoSpace = similarAartists[a].replace(" ", "%20");
-    bandsintown(); 
+    bandsArtistNoSpace = similarAartists[a].replace(" ", "%20");
+    eventful(); 
 }
 
 });
