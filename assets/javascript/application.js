95b7ab19c57c44a7dc73cdf58040bf939229791f
unknown <seljisri@hotmail.com>
2018-05-30 19:43:48 -0400

    updates to similar artists api section

--- assets/javascript/application.js
+++ assets/javascript/application.js
@@ -6,23 +6,30 @@ $(document).ready(function() {
     console.log("Ready!");
 
     var similarArtists = [];
+    var imageSrc = [];
 
     // The search Lastfm function takes an artist, searches the lastfm api for it, and then passes the data to createRow
     var searchLastfm = function(artist) {
-        var queryURL = "http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=" + artist + "&api_key=0bb42d7e989ca9d19b690353bc075069&format=json";
-        $.ajax({
-          url: queryURL,
-          method: "GET"
-        }).then(function(response) {
-         // createRow(response);
-            for (e=0; e<response.similarartists.artist.length; e++){
-                console.log(response.similarartists.artist[e].name);
-
-                similarArtists.push(response.similarartists.artist[e].name);
-            }
-
-
-        });
+        console.log("test");
+          var queryURL = "http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=" + artist + "&api_key=0bb42d7e989ca9d19b690353bc075069&format=json";
+          $.ajax({
+            url: queryURL,
+            method: "GET"
+          }).then(function(response) {
+  
+              var results = response.similarartists.artist;
+  
+  
+                  for (var i = 0; i < 5; i++)   {
+  
+                      similarArtists.push(results[i].name);
+                      imageSrc.push(results[i].image[2]["#text"]);
+                  }
+  
+                   console.log(similarArtists);
+                   console.log(imageSrc);
+  
+          })
       };
 
 
