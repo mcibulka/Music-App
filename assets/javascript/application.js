3848944437149a2bc25928af0d14e4b06bc7d1b4
unknown <seljisri@hotmail.com>
2018-06-03 17:32:11 -0400

    started cleanup

--- assets/javascript/application.js
+++ assets/javascript/application.js
@@ -2,11 +2,9 @@ var city = $("#city").val();
 var artist = $("#artist").val().trim();
 var country
 var eventArtistNoSpace
-//var bandsArtistNoSpace = artist.replace(" ", "%20") //changes spaces to bandsInTown's format
 var eventArtistNoSpace = artist.replace(" ", "+") //changes spaces to eventful's format
 
 $(document).ready(function() {
-    console.log("Ready!");
 
     var similarArtists = [];
     var imageSrc = [];
@@ -16,42 +14,36 @@ $(document).ready(function() {
 
         if (artist == "") {
                 $("#similarArtistRows").empty();
-                console.log("test");
+
                 var queryURL = "https://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=" + country + "&api_key=0bb42d7e989ca9d19b690353bc075069&format=json";
                 $.ajax({
                 url: queryURL,
                 method: "GET"
-                }).then(function(response) {    
+                }).then(function(response) {
+
                     var results = response.topartists.artist;
-    
-    
+        
                     for (var i = 0; i < 5; i++)   {
 
                         similarArtists.push(results[i].name);
                         imageSrc.push(results[i].image[2]["#text"]);
 
-                    var similarResult = $("<tr>");
+                        var similarResult = $("<tr>");
+                        var similarArtistCell = $("<code>");
+                        var similarArtistImg = $("<img>");       
+                        similarArtistImg.attr("src", results[i].image[2]["#text"])
 
-                    var similarArtistCell = $("<code>");
-                    var similarArtistImg = $("<img>");       
-                    similarArtistImg.attr("src", results[i].image[2]["#text"])
-                    console.log(results[i].image[2]["#text"]);
-
-                    var similarArtistResult = $("<td>");
-                    similarArtistResult.text(results[i].name)
+                        var similarArtistResult = $("<td>");
+                        similarArtistResult.text(results[i].name)
         
-                    similarArtistCell.append(similarArtistImg)
-                    similarResult.append(similarArtistCell, similarArtistResult)
-                    similarResult.appendTo($("#similarArtistRows"));
+                        similarArtistCell.append(similarArtistImg)
+                        similarResult.append(similarArtistCell, similarArtistResult)
+                        similarResult.appendTo($("#similarArtistRows"));
                     }
                 })
-
-                console.log(similarArtists);
-                console.log(imageSrc);
-            }
+        }
         else {
             $("#similarArtistRows").empty();
-                console.log("test");
                 var queryURL = "https://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=" + artist + "&api_key=0bb42d7e989ca9d19b690353bc075069&format=json";
                 $.ajax({
                     url: queryURL,
@@ -60,19 +52,15 @@ $(document).ready(function() {
         
                     var results = response.similarartists.artist;
         
-     
                         for (var i = 0; i < 5; i++)   {
         
                             similarArtists.push(results[i].name);
                             imageSrc.push(results[i].image[2]["#text"]);
 
-                            var similarResult = $("<tr>");
-
-            
+                            var similarResult = $("<tr>");          
                             var similarArtistCell = $("<code>");
                             var similarArtistImg = $("<img>");       
                             similarArtistImg.attr("src", results[i].image[2]["#text"])
-                            console.log(results[i].image[2]["#text"]);
 
                             var similarArtistResult = $("<td>");
                             similarArtistResult.text(results[i].name)
@@ -81,14 +69,15 @@ $(document).ready(function() {
                             similarResult.append(similarArtistCell, similarArtistResult)
                             similarResult.appendTo($("#similarArtistRows"));
                         }
-        
-                        console.log(similarArtists);
-                        console.log(imageSrc);
-        
                 });
         };
     };
-                
+
+    function addLastfmResults() {
+
+
+        
+    }    
     $("#search").on("click", function() {
         searchClicked();
         searchLastfm();
@@ -108,11 +97,11 @@ $(document).ready(function() {
         if (city == "") {
             eventfulURL = "https://api.eventful.com/json/events/search?app_key=BMHGt9rHhxJ8frMs&category=music&keywords="+eventArtistNoSpace+"&sort_order=date"
         }
-        //
+        
         else if ((city != "") && (eventArtistNoSpace == "")) {
             eventfulURL = "https://api.eventful.com/json/events/search?app_key=BMHGt9rHhxJ8frMs&category=music&location="+eventCityNoSpace+"&within=60&date=today&sort_order=popularity"
         }
-        //
+        
         else if (city != "") {
             eventfulURL = "https://api.eventful.com/json/events/search?app_key=BMHGt9rHhxJ8frMs&category=music&keywords="+eventArtistNoSpace+"&location="+eventCityNoSpace+"&within=60&sort_order=relevence"
         }
@@ -126,98 +115,52 @@ $(document).ready(function() {
                 "Allow-Control": "true",
                 "Cache-Control": "no-cache"
             }
-    }).then(function(response) {
-        if  (response.total_items < 1) {
-            var event = $("<tr>)");
-            event.text("No concerts found")
-            event.appendTo($("#eventsRows"));
-
-        }
-        else {
-            for (e = 0; e < response.events.event.length; e++) {
-                var event = $("<tr>");
-
-
-                var eventPlaying = $("<td>");
-                eventPlaying.text()
-                eventPlaying.text(response.events.event[e].title)
-
-                var eventCity = $("<td>");
-                eventCity.text(response.events.event[e].city_name+", "+response.events.event[e].region_abbr+", "+response.events.event[e].country_abbr)
-                country = response.events.event[e].country_name
-
-                //var eventVenue = $("<td>");
-                //eventVenue.text(response.events.event[e].venue_name)
-
-                var eventAddress = $("<td>");
-                eventAddress.text(response.events.event[e].venue_name)
-
-                var eventDate = $("<td>");
-
-                //Below code for Date without Time
-                //var dateWithoutTime = response.events.event[e].start_time
-                //dateWithoutTime = dateWithoutTime.substring(0, dateWithoutTime.indexOf(" "));
-                //eventDate.text(dateWithoutTime)
-
-                eventDate.text(response.events.event[e].start_time)
-                
-                event.append(eventPlaying, eventCity, 
-                    //eventVenue, 
-                    eventAddress, eventDate)
+        }).then(function(response) {
+            if  (response.total_items < 1) {
+                var event = $("<tr>)");
+                event.text("No concerts found")
                 event.appendTo($("#eventsRows"));
+
             }
-        }
+            else {
+                for (e = 0; e < response.events.event.length; e++) {
+                    var event = $("<tr>");
+
+
+                    var eventPlaying = $("<td>");
+                    eventPlaying.text()
+                    eventPlaying.text(response.events.event[e].title)
+
+                    var eventCity = $("<td>");
+                    eventCity.text(response.events.event[e].city_name+", "+response.events.event[e].region_abbr+", "+response.events.event[e].country_abbr)
+                    country = response.events.event[e].country_name
+
+                    //var eventVenue = $("<td>");
+                    //eventVenue.text(response.events.event[e].venue_name)
+
+                    var eventAddress = $("<td>");
+                    eventAddress.text(response.events.event[e].venue_name)
+
+                    var eventDate = $("<td>");
+
+                    //Below code for Date without Time
+                    //var dateWithoutTime = response.events.event[e].start_time
+                    //dateWithoutTime = dateWithoutTime.substring(0, dateWithoutTime.indexOf(" "));
+                    //eventDate.text(dateWithoutTime)
+
+                    eventDate.text(response.events.event[e].start_time)
+                    
+                    event.append(eventPlaying, eventCity, 
+                        eventAddress, eventDate)
+                    event.appendTo($("#eventsRows"));
+                }
+            }
+        })
+    }
+
+    $("#similarArtistRows").on("click", ".similar", function() {
+        var similarClicked = $(this).attr("artist");
+        $("#artist").val(similarClicked);
+        searchClicked()
     })
-}
-
-$("#similarArtistRows").on("click", ".similar", function() {
-    var similarClicked = $(this).attr("artist");
-    $("#artist").val(similarClicked);
-    searchClicked()
-})
-
-
-
-// function bandsintown() {
-//     var bandsURL = "rest.bandsintown.com/artists/"+bandsArtistNoSpace+"/events?app_id="
-
-//     $.ajax ({
-//         url: bandsURL,
-//         method: "GET"
-//     }).then(function(response) {
-//         for (s=0; s<response.length; s++) {
-//             var town = response.venue.city;
-//             if (town ==city) {
-//                 console.log(response[s].venue.name);
-//                 console.log(response[s].datetime);
-//             }
-//         }
-//     })
-// }
-
-// ---------------------
-
-
-
-    // for (a= 0; a < similarArtists.length; a++) {
-    //     //bandsArtistNoSpace = similarAartists[a].replace(" ", "%20");
-    //     artist = similarArtists[a].trim();
-    //     //var bandsArtistNoSpace = artist.replace(" ", "%20") //changes spaces to bandsInTown's format
-    //     var eventArtistNoSpace = artist.replace(" ", "+") //changes spaces to eventful's format
-        
-    //     var event = $("<tr>");
-
-    //     var similar = $("<td>");
-    //     similar.text(`${similarArtists[a]}`)
-    //     similar.attr("class", "similar")
-    //     similar.attr("id", similarArtists[a])
-            
-    //     event.append(similar)
-    //     event.appendTo($("#simlar-artists"));
-    // }
-
-// $(".similar").on("click", function() {
-//     ("#artist").val($(this).attr("id"));
-// })
-
 });
